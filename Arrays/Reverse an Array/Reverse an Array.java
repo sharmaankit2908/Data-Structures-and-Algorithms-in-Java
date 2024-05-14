e359a5775ef500996e59cabd5d738aea67544b17
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2024-05-14 12:08:21 +0530

    Add files via upload

---
+++ Arrays/Reverse an Array/Reverse an Array.java
@@ -0,0 +1,13 @@
+public class Solution {
+    public static int[] reverseArray(int n, int []nums) {
+        // Write your code here.
+        int i=0;
+        int j=nums.length-1;
+        while (i <j){
+            int c=nums[i];
+            nums[i]=nums[j];
+            nums[j]=c;
+            i++;
+            j--;
+        }
+        return nums;
