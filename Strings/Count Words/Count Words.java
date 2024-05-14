199ae5e29f923c9d688c43f82b3db9039aecc5ce
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2024-05-14 12:37:34 +0530

    Add files via upload

---
+++ Strings/Count Words/Count Words.java
@@ -0,0 +1,16 @@
+public class Solution {
+
+	public static int countWords(String str) {	
+		//Your code goes here
+		int count=0;
+		// for (int i=0;i<str.length();i++){
+		// 	if (str.charAt(i)==' '){
+		// 		count++;
+		// 	}
+		// }
+		// return ++count;
+		String s[]=str.split(" ");
+		return s.length;
+	}
+
+}
