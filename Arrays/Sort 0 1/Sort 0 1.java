3ebe399c9380946608b630bcf85f673b001eeb57
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2024-05-14 12:02:24 +0530

    Add files via upload

---
+++ Arrays/Sort 0 1/Sort 0 1.java
@@ -0,0 +1,18 @@
+public class Solution {  
+
+    public static void sortZeroesAndOne(int[] arr) {
+    	//Your code goes here
+        int count=0;
+        for (int i=0;i<arr.length;i++){
+            if (arr[i]==0){
+                count +=1;
+            }
+        }
+        for (int i=0;i<count;i++){
+            arr[i]=0;
+        }
+        for (int i=count;i<arr.length;i++){
+            arr[i]=1;
+        }
+    }
+}
