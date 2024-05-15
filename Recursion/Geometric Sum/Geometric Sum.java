e339971c88210c61454d94064ac40cc1d500c976
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2024-05-15 13:05:17 +0530

    Update Geometric Sum.java

--- Recursion/Geometric Sum/Geometric Sum.java
+++ Recursion/Geometric Sum/Geometric Sum.java
@@ -17,3 +17,25 @@ public class solution {
 
 	}
 }
+//public class solution {
+// 	public static double finpower(int k){
+// 		if (k==1){
+// 			return 0.5;
+// 		}
+// 		return (0.5*finpower(k-1));
+
+// 	}
+
+// 	public static double findGeometricSum(int k){
+// 		// Write your code here
+// 		if (k==0){
+// 			return 1;
+// 		}
+// 		else if (k==1){
+// 			return 1.5;
+// 		}
+// 		// return (findGeometricSum(k-1)+Math.pow(0.5, k));
+// 		return (findGeometricSum(k-1)+finpower(k));
+
+// 	}
+// }
