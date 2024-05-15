ea81c64fc7274799612c64209730f7a847784a11
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2024-05-15 17:12:21 +0530

    Add files via upload

---
+++ Linked List/MergeSort of LL/MergeSort of LL.java
@@ -0,0 +1,99 @@
+/*
+
+    Following is the Node class already written for the Linked List
+
+    class Node<T> {
+        T data;
+        Node<T> next;
+    
+        public Node(T data) {
+            this.data = data;
+        }
+    }
+
+*/
+
+public class Solution {
+    public static Node<Integer> mergeTwoSorteds(Node<Integer> head1, Node<Integer> head2) {
+        //Your code goes here
+
+        if (head1==null && head2==null){
+            return head1;
+        }
+        else if (head1==null && head2 !=null){
+            return head2;
+        }
+        else if (head1 !=null && head2==null){
+            return head1;
+        }
+        else{
+            Node<Integer> temp1=head1;
+            Node<Integer> temp2=head2;
+            Node<Integer> final_head;
+            if (temp1.data <temp2.data){
+                final_head=temp1;
+                temp1=temp1.next;
+            }
+            else {
+                final_head=temp2;
+                temp2=temp2.next;
+            }
+            Node<Integer> temp3=final_head;
+
+            while (temp1 !=null && temp2 !=null){
+                if (temp1.data < temp2.data){
+                    temp3.next=temp1;
+                    temp3=temp3.next;
+                    temp1=temp1.next;
+                }
+                else {
+                    temp3.next=temp2;
+                    temp3=temp3.next;
+                    temp2=temp2.next;
+                }
+            }
+            
+            if (temp1 !=null){
+                temp3.next=temp1;
+            }
+
+            if (temp2 !=null){
+                temp3.next=temp2;
+            }
+            return final_head;     
+        }
+    }
+
+	public static Node<Integer> mergeSort(Node<Integer> head) {
+		//Your code goes here
+        if (head == null || head.next == null){
+            return head;
+        }
+        int l=0;
+        Node<Integer> temp=head;
+        
+        while (temp !=null){
+            temp=temp.next;
+            l++;
+        }
+        int mid=l/2;
+        if (l%2==0){
+            mid=mid-1;
+        }
+        int count=0;
+        temp=head;
+        while (count <mid){
+            temp=temp.next;
+            count++;
+        }
+        Node<Integer> head2=temp.next;
+        temp.next=null;
+
+        Node<Integer> h1=mergeSort(head);
+        Node<Integer> h2=mergeSort(head2);
+        Node<Integer> final_ans=mergeTwoSorteds(h1, h2);
+        return final_ans; 
+        
+	}
+
+}
