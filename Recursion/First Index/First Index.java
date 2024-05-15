0d13adb262b69b79625c701242c0d6230de3316d
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2024-05-15 10:31:05 +0530

    Add files via upload

---
+++ Recursion/First Index/First Index.java
@@ -0,0 +1,24 @@
+public class Solution {
+
+	public static int firstIndex1(int input[], int x,int s) {
+		if (s==input.length-1 && input[s] !=x){
+			return -1;
+		}
+		if (input[s]==x){
+			return s;
+		}
+		return firstIndex1(input, x,s+1);
+	}
+
+	public static int firstIndex(int input[], int x) {
+		/* Your class should be named Solution
+		 * Don't write main().
+		 * Don't read input, it is passed as function argument.
+		 * Return output and don't print it.
+	 	 * Taking input and printing output is handled automatically
+		*/
+
+		return firstIndex1(input, x,0);	
+	}
+	
+}
