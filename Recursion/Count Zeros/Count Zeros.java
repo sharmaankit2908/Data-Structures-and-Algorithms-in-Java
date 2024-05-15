4cddf1ae02bc209677487fa2c9bac4d37e8eae1c
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2024-05-15 13:08:35 +0530

    Update Count Zeros.java

--- Recursion/Count Zeros/Count Zeros.java
+++ Recursion/Count Zeros/Count Zeros.java
@@ -24,3 +24,35 @@ public class solution {
 
 	}
 }
+
+
+// public class solution {
+
+// 	public static int countZerosRec(int input){
+// 		// Write your code here
+// 		if (input<10){
+// 			if (input==0){
+// 				return 1;
+// 			}
+// 			else {
+// 				return 0;
+// 			}
+// 		}
+// 		else if (input <100 && input >=10){
+// 			if (input %10==0){
+// 				return 1;
+// 			}
+// 			else {
+// 				return 0;
+// 			}
+// 		}
+// 		int b=input%10;
+// 		if (b==0){
+// 			return (1+countZerosRec(input/10));
+// 		}
+// 		else {
+// 			return countZerosRec(input/10);
+// 		}
+		
+// 	}
+// }
