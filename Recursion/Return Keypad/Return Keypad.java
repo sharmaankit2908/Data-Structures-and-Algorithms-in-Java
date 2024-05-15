c2106cc15bdfbd4381bd69b12d7cf33d053f324a
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2024-05-15 12:20:29 +0530

    Add files via upload

---
+++ Recursion/Return Keypad/Return Keypad.java
@@ -0,0 +1,72 @@
+
+
+public class solution {
+
+	public static String get(int a){
+		String ans;
+		if (a==0 || a==1){
+			ans="";
+		}
+		else if (a==2){
+			ans="abc";
+		}
+		else if(a==3){
+			ans="def";
+		}
+		else if(a==4){
+			ans="ghi";
+		}
+		else if(a==5){
+			ans= "jkl";
+		}
+		else if(a==6){
+			ans="mno";
+		}
+		else if(a==7){
+			ans="pqrs";	
+		}
+		else if(a==8){
+			ans="tuv";
+		}
+		else {
+			ans="wxyz";
+		}
+		return ans;
+	}
+
+	// Return a string array that contains all the possible strings
+	public static String[] keypad(int n){
+		// Write your code here
+		if (n<10){
+			String b1=get(n);
+			String ans[]=new String[b1.length()];
+			for (int i=0;i<b1.length();i++){
+				ans[i]=String.valueOf(b1.charAt(i));
+			}
+			return ans;
+		}
+		String m1[]=keypad(n/10);
+		String m2[]=keypad(n%10);
+		if (m1.length>0 && m2.length >0){
+			String ans[]=new String[m1.length*m2.length];
+			int k=0;
+			for (int i=0;i<m1.length;i++){
+				for (int j=0;j<m2.length;j++){
+					ans[k]=m1[i]+m2[j];
+					k++;
+				}
+			}
+			return ans;
+		}
+		else if (m1.length>0){
+			return m1;
+		}
+		else if (m2.length >0){
+			return m2;
+		}
+		else {
+			return new String[0];
+		}
+	}
+	
+}
