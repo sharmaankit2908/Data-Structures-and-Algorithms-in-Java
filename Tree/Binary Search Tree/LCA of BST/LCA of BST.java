515b5a0ed92d0c6534ecfb0c432b9353f5776865
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2024-05-16 10:46:59 +0530

    Add files via upload

---
+++ Tree/Binary Search Tree/LCA of BST/LCA of BST.java
@@ -0,0 +1,27 @@
+public class Solution {
+
+	/*
+	 * Binary Tree Node class
+	 * 
+	 * class BinaryTreeNode<T> { T data; BinaryTreeNode<T> left; BinaryTreeNode<T> right;
+	 * 
+	 * public BinaryTreeNode(T data) { this.data = data; } }
+	 */
+
+
+	
+	public static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
+		if (root==null){
+			return -1;
+		}
+		if (root.data<a && root.data<b){
+			return getLCA(root.right, a, b);
+		}
+		else if (root.data>a && root.data>b){
+			return getLCA(root.left, a, b);
+
+		}
+		return root.data;
+		
+	}
+}
