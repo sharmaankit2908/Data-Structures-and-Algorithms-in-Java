b5fc29b13c1e32f736fa02874f28a850e1acc0dc
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2024-05-16 23:12:37 +0530

    Update Return Keypad.java

--- Recursion/Return Keypad/Return Keypad.java
+++ Recursion/Return Keypad/Return Keypad.java
@@ -70,3 +70,61 @@ public class solution {
 	}
 	
 }
+
+// public class solution {
+
+// 	// Return a string array that contains all the possible strings
+// 		public static String get(int a){
+// 			String ans;
+// 			if (a==0 || a==1){
+// 				ans=" ";
+// 			}
+// 			else if (a==2){
+// 				ans="abc";
+// 			}
+// 			else if(a==3){
+// 				ans="def";
+// 			}
+// 			else if(a==4){
+// 				ans="ghi";
+// 			}
+// 			else if(a==5){
+// 				ans= "jkl";
+// 			}
+// 			else if(a==6){
+// 				ans="mno";
+// 			}
+// 			else if(a==7){
+// 				ans="pqrs";	
+// 			}
+// 			else if(a==8){
+// 				ans="tuv";
+// 			}
+// 			else {
+// 				ans="wxyz";
+// 			}
+// 			return ans;
+// 		}
+// 	public static String[] keypad(int n){
+// 		// Write your code here
+// 		if (n==0){
+// 			String[] ans=new String[1];
+// 			ans[0]="";
+// 			return ans;
+// 		}
+// 		String[] small_output=keypad(n/10);
+// 		int m=n%10;
+// 		String s=get(m);	
+// 		String[] final_ans=new String[s.length()*small_output.length];
+// 		int k=0;
+// 		for (int i=0;i<small_output.length;i++){
+// 			for (int j=0;j<s.length();j++){
+// 				final_ans[k++]=small_output[i]+s.charAt(j);
+// 			}
+// 		}
+// 		return final_ans;
+
+// 	}
+	
+// }
+
