d10dcccccd2539a976d018d82b286e6472412e62
Ankit Sharma <123895014+sharmaankit2908@users.noreply.github.com>
2024-05-17 12:37:35 +0530

    Update Shortest Common Supersequence.java

--- Dynamic Programming/Shortest Common Supersequence/Shortest Common Supersequence.java
+++ Dynamic Programming/Shortest Common Supersequence/Shortest Common Supersequence.java
@@ -1,52 +1,82 @@
-import java.util.* ;
-import java.io.*; 
+
+
 public class Solution {
 
-    public static String helper(String a, String b,int n1, int n2,int index1,int index2, String res,int[][] as,String[][] dp) {
-        if (index1==n1 && index2==n2){
-            return res;
-        }
-        else if (index1==n1){
+    private static String backtrack(int[][] dp, String a, String b) {
+        String ans = "";
+        int n = a.length(), m = b.length();
+
+        // Bactracking by traversing from state dp[n][m] to dp[0][0].
+        while(n > 0 && m > 0) {
             
-            return res+b.substring(index2);
-        }
-        else if(index2==n2){
-            return res+a.substring(index1);
-        }
-        if (as[index1][index2] !=-1){
-            return dp[index1][index2];
-        }
-        String ans;
-        if (a.charAt(index1)==b.charAt(index2)){
-            ans =helper(a, b, n1, n2, index1+1, index2+1, res+a.charAt(index1),as,dp);
-        }
-        else{
-            String s1=helper(a, b, n1, n2, index1+1, index2, res+a.charAt(index1),as,dp);
-            String s2=helper(a, b, n1, n2, index1, index2+1, res+b.charAt(index2),as,dp);
-            if (s1.length()<s2.length()){
-                ans= s1;
+            if(a.charAt(n - 1) == b.charAt(m - 1)) {
+                ans += a.charAt(n - 1);
+                n--;
+                m--;
             }
-            else{
-                ans= s2;
+
+            else if(dp[n-1][m] < dp[n][m-1]) {
+                ans += a.charAt(n-1);
+                n--;
+            }
+            
+            else {
+                ans += b.charAt(m-1);
+                m--;
             }
         }
-        as[index1][index2]=1;
-        dp[index1][index2]=ans;
-        return ans;
-    }
-    public static String shortestSupersequence(String a, String b) {
-        // Write your code here..
-        int n1=a.length();
-        int n2=b.length();
-        String[][] dp=new String[n1][n2];
-        int[][] as=new int[n1][n2];
-        for (int i=0;i<n1;i++){
-            for (int j=0;j<n2;j++){
-                as[i][j]=-1;
-            }
+
+        while(n > 0) {
+            ans += a.charAt(n-1);
+            n--;
         }
-        String res="";
-        return helper(a, b, n1, n2, 0,0, res,as,dp);
+
+        while(m > 0) {
+            ans += b.charAt(m-1);
+            m--;
+        }
+
+        // As we retrieved characters in reverse we need to reverse it before returning.
+        String answer = "";
+        for (int i = 0; i < ans.length(); i++) {
+            answer = ans.charAt(i) + answer;
+        }
+        return answer;
     }
 
+    public static String shortestSupersequence(String a, String b) {
+        // Get lenghts of a and b in n and m.
+        int n = a.length(), m = b.length();
+
+        // Declare DP table and dpChar.
+        int dp[][] = new int[n + 1][m + 1];
+
+        // Initialize the first column.
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = i;
+        }
+
+        // Initialize the first row.
+        for (int i = 0; i <= m; i++) {
+            dp[0][i] = i;
+        }
+
+        // Fill dp table with increasing order of rows and columns.
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+
+                // If the last characters of prefixes are equal.
+                if (a.charAt(i - 1) == b.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                }
+
+                else {
+                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + 1;
+                }
+            }
+        }
+
+        // Using the dp table backtrack the characters used in supersequence.
+        return backtrack(dp, a, b);
+    }
 }
